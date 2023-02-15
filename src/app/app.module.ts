c50d58ee2ac04d94cd2296cbe8a5a57ca8f190af
Tan Chee Kang <89293000+cktan2511@users.noreply.github.com>
2023-02-16 01:15:46 +0500

    init files

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
